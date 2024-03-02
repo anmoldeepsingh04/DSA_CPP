16a0484d9bb35bbf2bda0d6f8094dd2059b843e4
anmoldeepsingh04 <anmoldeepcrux@gmail.com>
2024-03-02 19:09:49 +0000

    New Addition

---
+++ debugExerciseWeek1/ques1.cpp
@@ -0,0 +1,19 @@
+// Debug the code. Take input a character, print 1, if its a 
+// capital alphabet, print 0, if its a lowercase alphabet, else print -1.
+
+#include<iostream> 
+using namespace std;
+
+int main() {
+	char c;
+    cin>>c;
+    if(97 <=c && c<= 97 + 'z'){
+        cout<<0<<endl;
+    }
+	else if(65 <=c && c<= 65 + 'Z'){
+        cout<<1<<endl;
+    }
+    else{
+        cout<<-1<<endl;
+    }
+}
