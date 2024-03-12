677f21093049a9e8f1007cb9d751b940c211e454
anmoldeepsingh04 <anmoldeepcrux@gmail.com>
2024-03-12 18:11:12 +0000

    Time complexity description

--- DSA/3.arraysTimeAndSpaceComplexity/introToTime&SpaceComplexity.cpp
+++ DSA/3.arraysTimeAndSpaceComplexity/introToTime&SpaceComplexity.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 
 using namespace std;
-
+// Time complexity is used to estimate the runtime of an algorithm as the number of inputs increase
+// The time that we talk about in time complexity is not the usual time rather the number of operations 
+// that are performed in an algorithm
 int main(){
     // what is the time complexity of the code below?
     int a = 0, b = 0, n;
